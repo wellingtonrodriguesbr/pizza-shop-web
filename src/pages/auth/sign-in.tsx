fb52679dc6aa5356478599f3952e9c8d6befdf87
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-01-09 07:57:35 -0300

    feat: sign in screen

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,10 +1,35 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { Helmet } from "react-helmet-async";
 
 export function SignIn() {
   return (
     <>
       <Helmet title="Login" />
-      <h1>Sign in</h1>
+      <div className="p-8">
+        <div className="w-[340px] flex flex-col justify-end gap-6">
+          <div className="flex flex-col gap-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Acessar painel
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Acompanhe suas vendas pelo painel do parceiro
+            </p>
+          </div>
+
+          <form className="space-y-4">
+            <div className="space-y-2">
+              <Label htmlFor="email">Seu e-mail</Label>
+              <Input id="email" type="email" />
+            </div>
+
+            <Button className="w-full" type="submit">
+              Acessar painel
+            </Button>
+          </form>
+        </div>
+      </div>
     </>
   );
 }
