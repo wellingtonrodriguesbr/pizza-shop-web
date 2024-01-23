b1ad757b5fdb4624471f7de8139e9841369a2948
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-01-23 06:51:42 -0300

    feat: 404 page

--- src/router.tsx
+++ src/router.tsx
@@ -6,11 +6,13 @@ import { Dashboard } from "./pages/app/dashboard/dashboard";
 import { SignIn } from "./pages/auth/sign-in";
 import { SignUp } from "./pages/auth/sign-up";
 import { Orders } from "./pages/app/orders/orders";
+import { NotFound } from "./pages/404";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         path: "/",
