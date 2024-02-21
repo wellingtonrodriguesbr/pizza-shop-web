99996a9d438a9ab4cd245694bf0de47f82cc4f70
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2024-02-21 08:06:35 -0300

    chore: update condition

--- src/pages/app/dashboard/day-orders-amount-card.tsx
+++ src/pages/app/dashboard/day-orders-amount-card.tsx
@@ -24,7 +24,7 @@ export function DayOrdersAmountCard() {
         <span className="text-2xl font-bold tracking-tight">
           {data?.amount.toLocaleString("pt-BR")}
         </span>
-        {data.diffFromYesterday >= 0 ? (
+        {data.diffFromYesterday > 0 ? (
           <p className="text-xs text-muted-foreground">
             <span className="text-emerald-500 dark:text-emerald-400">
               +{data?.diffFromYesterday}%
